5cb9015d07826f385c11836c6c1656b8c569e4ef
Tomas <tomasnyiri@gmail.com>
2021-12-13 13:34:21 +0100

    Pridanie prvych pismen na odskusanie
    Dorobenie prvych funkcii na odskusanie pismen A-F

--- Core/Inc/display.h
+++ Core/Inc/display.h
@@ -2,7 +2,7 @@
  * Display.h
  *
  *  Created on: 13. 12. 2021
- *      Author: Author: Molnar & Nyiri
+ *      Author: Molnar & Nyiri
  */
 
 #ifndef DISPLAY_H_
@@ -21,6 +21,23 @@
 void setAllSegments(void);
 void resetAllSegments(void);
 void resetAllDigits(void);
+
+void setA(void);
+void seta(void);
+void setb(void);
+void setC(void);
+void setc(void);
+void setd(void);
+void setE(void);
+void setF(void);
+
+void setK(void);
+void setM(void);
+void setV(void);
+void setW(void);
+void setX(void);
+void setZ(void);
+
 
 
 #endif /* DISPLAY_H_ */
--- Core/Src/display.c
+++ Core/Src/display.c
@@ -38,5 +38,107 @@ void resetAllDigits(void)
 	LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_7);
 }
 
+void setA(void){
+	LL_GPIO_ResetOutputPin(GPIOA, segmentA_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentB_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentC_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentE_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentF_Pin);
+	LL_GPIO_ResetOutputPin(GPIOB, segmentG_Pin);
+	// reset D
+	LL_GPIO_SetOutputPin(GPIOB, segmentD_Pin);
+
+}
+void seta(void){
+	LL_GPIO_ResetOutputPin(GPIOA, segmentA_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentB_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentC_Pin);
+	LL_GPIO_ResetOutputPin(GPIOB, segmentD_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentE_Pin);
+	LL_GPIO_ResetOutputPin(GPIOB, segmentG_Pin);
+	// reset f
+	LL_GPIO_SetOutputPin(GPIOA, segmentF_Pin);
+
+}
+void setb(void){
+	LL_GPIO_ResetOutputPin(GPIOA, segmentC_Pin);
+	LL_GPIO_ResetOutputPin(GPIOB, segmentD_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentE_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentF_Pin);
+	LL_GPIO_ResetOutputPin(GPIOB, segmentG_Pin);
+	// reset a,b
+	LL_GPIO_SetOutputPin(GPIOA, segmentA_Pin);
+	LL_GPIO_SetOutputPin(GPIOA, segmentB_Pin);
+}
+void setC(void){
+	LL_GPIO_ResetOutputPin(GPIOA, segmentA_Pin);
+	LL_GPIO_ResetOutputPin(GPIOB, segmentD_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentE_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentF_Pin);
+	// reset B,C,G
+	LL_GPIO_SetOutputPin(GPIOA, segmentB_Pin);
+	LL_GPIO_SetOutputPin(GPIOA, segmentC_Pin);
+	LL_GPIO_SetOutputPin(GPIOB, segmentG_Pin);
+}
+void setc(void){
+	LL_GPIO_ResetOutputPin(GPIOB, segmentD_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentE_Pin);
+	LL_GPIO_ResetOutputPin(GPIOB, segmentG_Pin);
+	//reset
+	LL_GPIO_SetOutputPin(GPIOA, segmentA_Pin);
+	LL_GPIO_SetOutputPin(GPIOA, segmentB_Pin);
+	LL_GPIO_SetOutputPin(GPIOA, segmentC_Pin);
+	LL_GPIO_SetOutputPin(GPIOA, segmentF_Pin);
+}
+void setd(void){
+	LL_GPIO_ResetOutputPin(GPIOA, segmentB_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentC_Pin);
+	LL_GPIO_ResetOutputPin(GPIOB, segmentD_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentE_Pin);
+	LL_GPIO_ResetOutputPin(GPIOB, segmentG_Pin);
+	//reset
+	LL_GPIO_SetOutputPin(GPIOA, segmentA_Pin);
+	LL_GPIO_SetOutputPin(GPIOA, segmentF_Pin);
+}
+void setE(void){
+	LL_GPIO_ResetOutputPin(GPIOA, segmentA_Pin);
+	LL_GPIO_ResetOutputPin(GPIOB, segmentD_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentE_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentF_Pin);
+	LL_GPIO_ResetOutputPin(GPIOB, segmentG_Pin);
+	//reset
+	LL_GPIO_SetOutputPin(GPIOA, segmentB_Pin);
+	LL_GPIO_SetOutputPin(GPIOA, segmentC_Pin);
+}
+void setF(void){
+	LL_GPIO_ResetOutputPin(GPIOA, segmentA_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentE_Pin);
+	LL_GPIO_ResetOutputPin(GPIOA, segmentF_Pin);
+	LL_GPIO_ResetOutputPin(GPIOB, segmentG_Pin);
+	//reset
+	LL_GPIO_SetOutputPin(GPIOA, segmentB_Pin);
+	LL_GPIO_SetOutputPin(GPIOA, segmentC_Pin);
+	LL_GPIO_SetOutputPin(GPIOB, segmentD_Pin);
+}
+
+// Disgusting letters :D
+void setK(void){
+
+}
+void setM(void){
+
+}
+void setV(void){
+
+}
+void setW(void){
+
+}
+void setX(void){
+
+}
+void setZ(void){
+
+}
 
 
